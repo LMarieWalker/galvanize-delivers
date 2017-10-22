60547f7be9e017121f19e9f942e0d9e7d1ae35e9
Rod <LourdesMarieWalker@gmail.com>
2017-10-21 21:18:19 -0700

    add css and js files

---
+++ js/app.js
@@ -0,0 +1,27 @@
+// create a list of orders everytime a menu item button is pushed
+
+$('burgerButtonClick').click(function(){
+  
+})
+
+// START EXAMPLE
+document.body.onload = addElement;
+
+function addElement () {
+  let theDiv = document.createElement("div");
+  theDiv.innerHTML = "Some placeholder text";
+  document.body.appendChild(theDiv);
+}
+// END EXAMPLE
+
+
+// ----------------------------
+// create a function that will add the individual orders to the order summary table
+// table id = placedOrders
+
+
+
+
+// ----------------------------
+// create a function that will update the document
+// create a function that will take the final order with the form fill
